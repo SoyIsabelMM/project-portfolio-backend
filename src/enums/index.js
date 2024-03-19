a607b1e2e24f41fb3940f90a02d159bca1ec2772
Isabel Martinez <isabelmartinez19.30@gmail.com>
2024-03-18 21:27:37 -0300

    feat: agregar enums

---
+++ src/enums/index.js
@@ -0,0 +1,25 @@
+const HttpStatus = Object.freeze({
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+});
+
+const HttpResponseMessage = Object.freeze({
+  SUCCESS: 'Success',
+  CREATED: 'Resource created successfully',
+  ALREADY_EXISTS: 'Resource already exists',
+  BAD_REQUEST: 'Bad Request',
+  UNAUTHORIZED: 'Unauthorized access',
+  FORBIDDEN: 'Forbidden access',
+  NOT_FOUND: 'Resource not found',
+  INTERNAL_SERVER_ERROR: 'Internal server error',
+});
+
+module.exports = {
+  HttpStatus,
+  HttpResponseMessage,
+};
